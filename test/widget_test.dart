7dcafd05f9b26fb22437c8e876231f737d08a607
Blake Barrett <blake@blakebarrett.net>
2022-12-29 21:30:18 -0800

    More updates

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:l2/main.dart';
+import 'package:lore/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LoreApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
