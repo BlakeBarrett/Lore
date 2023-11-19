eeae40f782705b5b4e6f2941ee06e029aacbe91f
Blake Barrett <blake@blakebarrett.net>
2023-11-18 18:31:19 -0800

    Looks like a thing. Runs on Windows.

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -2,11 +2,21 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.NavigationRail
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.blakebarrett.extensions.DropDataResult
@@ -27,36 +38,29 @@ import com.blakebarrett.extensions.onTextDrop
 fun App() {
     MaterialTheme {
         var droppedDataResultList by remember { mutableStateOf<List<DropDataResult>?>(value = null) }
-        Column (modifier = Modifier
+        Scaffold (modifier = Modifier
             .fillMaxSize()
             .onFileDrop(true) {
                 droppedDataResultList = it
             }
             .onTextDrop(true) {
                 droppedDataResultList = it
-            },
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            droppedDataResultList?.let {
-                DroppedItemFeed(items = it, modifier = Modifier.fillMaxWidth())
             }
-//            repeat(droppedDataResultList?.size ?: 0) { index ->
-//                droppedDataResultList?.get(index).let { dropData ->
-//                    Row(
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .background(when (dropData) {
-//                                null -> Color.Transparent
-//                                else -> Color.Magenta
-//                            }),
-//                        verticalAlignment = Alignment.CenterVertically
-//                    ) {
-//                        Card {
-//                            Text (text = "MD5Sum: ${dropData?.md5sum ?: "null"}")
-//                        }
-//                    }
-//                }
-//            }
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Column {
+                    Icon(Icons.Default.AccountCircle, contentDescription = "Profile", modifier = Modifier.padding(16.dp))
+                    Icon(Icons.Default.Search, contentDescription = "Search", modifier = Modifier.padding(16.dp))
+                }
+                NavigationRail(
+                    modifier = Modifier.fillMaxWidth(),
+                    content = {
+                        droppedDataResultList?.let {
+                            DroppedItemFeed(items = it, modifier = Modifier.fillMaxWidth())
+                        }
+                    }
+                )
+            }
         }
     }
 }
@@ -77,7 +81,7 @@ fun DroppedItemFeed(items: List<DropDataResult>, modifier: Modifier = Modifier) 
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication, title = "Lore") {
         App()
     }
 }
