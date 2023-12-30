8d57cf684227588c9431b6f8620e539c23d30081
Blake Barrett <blake@blakebarrett.net>
2023-12-29 20:51:57 -0800

    Fix test

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const LoreApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LoreApp extends StatelessWidget {
+  const LoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
