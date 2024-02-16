a72ba6fe4bdbf1b2baf945f6f9ccfe7396d4884d
Blake Barrett <blake@blakebarrett.net>
2024-02-15 16:19:07 -0800

    feat: Finally looks good with feature parity of the old approach.

--- lib/lore_app_bar.dart
+++ lib/lore_app_bar.dart
@@ -2,44 +2,44 @@ import 'dart:io';
 
 import 'package:Lore/artifact.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
-import 'package:file_icon/file_icon.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:like_button/like_button.dart';
 import 'package:regexpattern/regexpattern.dart';
 
 class LoreAppBar extends StatefulWidget {
-  const LoreAppBar(
+  LoreAppBar(
       {super.key,
       required this.artifact,
       required this.onOpenFileTap,
       required this.onSearch,
       required this.onFavoriteTap,
-      this.isFavorite});
+      required this.isFavorite});
 
   final Artifact? artifact;
   final Function() onOpenFileTap;
   final Function(String) onSearch;
   final Function() onFavoriteTap;
-  final bool? isFavorite;
+  bool isFavorite = false;
 
   @override
   State<LoreAppBar> createState() => _LoreAppBarState();
 }
 
 class _LoreAppBarState extends State<LoreAppBar> {
-  late Widget? image;
+  late Widget? previewBackgroundImage;
 
   final TextEditingController _animatedSearchBarTextConroller =
       TextEditingController();
 
   Widget? getBackgroundImage(final BuildContext context) {
-    if (image is SizedBox) return null;
+    if (previewBackgroundImage is SizedBox) return null;
     return ClipRRect(
       child: Stack(
         children: [
           SizedBox.expand(
-            child: image!,
+            child: previewBackgroundImage!,
           ),
           SizedBox.expand(
             child: Container(
@@ -48,10 +48,11 @@ class _LoreAppBarState extends State<LoreAppBar> {
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
                   colors: <Color>[
+                    Theme.of(context).colorScheme.inverseSurface,
                     Colors.transparent,
                     Colors.transparent,
                     Colors.transparent,
-                    Theme.of(context).colorScheme.background,
+                    Theme.of(context).colorScheme.inverseSurface,
                   ],
                 ),
               ),
@@ -62,9 +63,16 @@ class _LoreAppBarState extends State<LoreAppBar> {
     );
   }
 
-  Widget? getFlexibleSpace(final BuildContext context, final Artifact? artifact,
-      final bool hasImagePreview) {
+  Widget? getFlexibleSpace(
+      final BuildContext context,
+      final Artifact? artifact,
+      final bool hasImagePreview,
+      final bool isFavorite,
+      final Function? onFavoriteTap) {
+    final String name = artifact?.name ?? '';
+    final String md5sum = artifact?.md5sum ?? '';
     return FlexibleSpaceBar(
+      // stretchModes: const [StretchMode.blurBackground, StretchMode.zoomBackground],
       collapseMode: CollapseMode.parallax,
       expandedTitleScale: 1.0,
       titlePadding: const EdgeInsets.all(8),
@@ -72,13 +80,47 @@ class _LoreAppBarState extends State<LoreAppBar> {
       title: ListTile(
         iconColor: Theme.of(context).appBarTheme.toolbarTextStyle?.color,
         title: Text(
-          artifact?.name ?? '',
-          style: Theme.of(context).appBarTheme.toolbarTextStyle,
+          maxLines: 2,
+          overflow: TextOverflow.visible,
+          name,
+          style: Theme.of(context).primaryTextTheme.titleMedium,
         ),
         subtitle: SelectableText(
-          artifact?.md5sum ?? '',
-          style: Theme.of(context).appBarTheme.toolbarTextStyle,
+          md5sum,
+          style: Theme.of(context).primaryTextTheme.titleSmall,
         ),
+        trailing: (artifact != null)
+            ? SizedBox(
+                width: 41,
+                height: 41,
+                child: LikeButton(
+                  // circleSize: 24,
+                  // bubblesSize: 30,
+                  padding: const EdgeInsets.fromLTRB(0, 0, 8.0, 0),
+                  circleColor: CircleColor(
+                    start: Theme.of(context).iconTheme.color!,
+                    end: Theme.of(context).primaryIconTheme.color!,
+                  ),
+                  bubblesColor: BubblesColor(
+                    dotPrimaryColor: Theme.of(context).primaryIconTheme.color!,
+                    dotSecondaryColor:
+                        Theme.of(context).primaryIconTheme.color!,
+                  ),
+                  animationDuration: const Duration(milliseconds: 666),
+                  likeBuilder: (final bool isLiked) {
+                    return Icon(
+                      (isLiked) ? Icons.favorite : Icons.favorite_outline,
+                      color: Theme.of(context).primaryIconTheme.color,
+                    );
+                  },
+                  isLiked: widget.isFavorite,
+                  onTap: (final bool isLiked) async {
+                    onFavoriteTap?.call();
+                    return !isLiked;
+                  },
+                ),
+              )
+            : null,
       ),
       background: (hasImagePreview) ? getBackgroundImage(context) : null,
     );
@@ -104,11 +146,11 @@ class _LoreAppBarState extends State<LoreAppBar> {
       try {
         hasImagePreview = true;
         maxSize = Size(fullWidth, maxHeight);
-        image = Image.file(
+        previewBackgroundImage = Image.file(
           widget.artifact!.file!,
           fit: BoxFit.cover,
           height: fullHeight * 3,
-          errorBuilder: (context, error, stackTrace) {
+          errorBuilder: (final _, final __, final ___) {
             maxSize = Size(fullWidth, minHeight);
             hasImagePreview = false;
             return const SizedBox.shrink();
@@ -117,19 +159,17 @@ class _LoreAppBarState extends State<LoreAppBar> {
       } catch (e) {
         maxSize = Size(fullWidth, minHeight);
         hasImagePreview = true;
-        image = null;
+        previewBackgroundImage = null;
       }
     } else {
       maxSize = Size(fullWidth, minHeight);
-      image = null;
+      previewBackgroundImage = null;
     }
 
     return SliverAppBar(
       backgroundColor: Theme.of(context).primaryColor,
       iconTheme: Theme.of(context).primaryIconTheme,
       titleTextStyle: Theme.of(context).primaryTextTheme.titleLarge,
-      elevation: 2.0,
-      scrolledUnderElevation: 1.0,
       shape: Theme.of(context).appBarTheme.shape,
       pinned: true,
       snap: true,
@@ -171,26 +211,8 @@ class _LoreAppBarState extends State<LoreAppBar> {
           ),
         ),
       ],
-      flexibleSpace:
-          getFlexibleSpace(context, widget.artifact, hasImagePreview),
-      // flexibleSpace: FlexibleSpaceBar(
-      //   collapseMode: CollapseMode.parallax,
-      //   expandedTitleScale: 1.0,
-      //   titlePadding: const EdgeInsets.all(8),
-      //   centerTitle: false,
-      //   title: ListTile(
-      //     iconColor: Theme.of(context).appBarTheme.toolbarTextStyle?.color,
-      //     title: Text(
-      //       widget.artifact?.name ?? '',
-      //       style: Theme.of(context).appBarTheme.toolbarTextStyle,
-      //     ),
-      //     subtitle: SelectableText(
-      //       widget.artifact?.md5sum ?? '',
-      //       style: Theme.of(context).appBarTheme.toolbarTextStyle,
-      //     ),
-      //   ),
-      //   background: (hasImagePreview) ? getBackgroundImage(context) : null,
-      // ),
+      flexibleSpace: getFlexibleSpace(context, widget.artifact, hasImagePreview,
+          widget.isFavorite, widget.onFavoriteTap),
     );
   }
 }
